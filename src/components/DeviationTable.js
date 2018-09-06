1e24c30ec18dfbbdc4aa0866a16316d15c651555
Yury Romanov <yury.romanov@bk.ru>
2018-09-07 02:19:20 +0300

    Add table sorting

--- src/components/DeviationTable.js
+++ src/components/DeviationTable.js
@@ -3,12 +3,52 @@ import { tableHeaders, tableData } from '../data-source/DataSource';
 // import { Indicator } from './Indicator';
 
 export class DeviationTable extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = {
+         tableData: tableData,
+         sortBy: 3,
+         descending: false
+      };
+
+      this.handleHeaderClick = this.handleHeaderClick.bind(this);
+   }
+
+   handleHeaderClick(ev) {
+      var column = ev.target.cellIndex;
+
+      var descending = this.state.sortBy === column && !this.state.descending;
+      
+
+      var newTableData = [...this.state.tableData].sort((a,b) => {
+         if(descending) {
+            if(a[column] < b[column]) return 1;
+            if(a[column] > b[column]) return -1;
+         } else {
+            if(a[column] > b[column]) return 1;
+            if(a[column] < b[column]) return -1;   
+         }
+         return 0;
+      });
+
+      this.setState({
+         tableData: newTableData,
+         sortBy: column,
+         descending: descending
+      })
+   }
+   
    render() {
       const headersRow = tableHeaders.map((elem, index) => {
-         return <th key={index}>{elem}</th>;
+         let title = elem;
+         if(this.state.sortBy === index) {
+            title += (this.state.descending ? ' \u2191 ' : ' \u2193 ');
+         }
+         return <th key={index}>{title}</th>;
       });
       
-      const dataRows = tableData.map((elem, index) => {
+      const dataRows = this.state.tableData.map((elem, index) => {
          const dataCells = elem.map((elem, index) => {
             return <td key={index}>{elem}</td>; 
          });
@@ -17,7 +57,7 @@ export class DeviationTable extends React.Component {
 
       return (
          <table className="DeviationTable">
-            <thead>
+            <thead onClick={this.handleHeaderClick}>
                <tr>{headersRow}</tr>
             </thead>
             <tbody>{dataRows}</tbody>
--- src/data-source/DataSource.js
+++ src/data-source/DataSource.js
@@ -1,5 +1,12 @@
 import data from './data.json';
 
+const getMaxAbs = function(arr) {
+   const sortedArr = Array.from(rawTableData).sort((a,b) => {
+      return Math.abs(b.fDeltaPlan) - Math.abs(a.fDeltaPlan)
+   });
+   return sortedArr[0].fDeltaPlan;
+};
+
 const rawtableHeaders = data.fa.fa_data.axis.r;
 const rawTableData = data.fa.fa_data.r;
 
@@ -9,11 +16,14 @@ const tableHeaders = [
    'Отклонение от плана, П.П.' 
 ]; 
 
+const maxAbs = getMaxAbs(rawTableData);
+
 const tableData = rawTableData.map(elem => {
    return [ 
       ...elem.axis.r.map(elem => elem.sName_RU), 
       elem.sMeasDelta_RU, 
-      elem.fDeltaPlan 
+      // maxAbs/elem.fDeltaPlan
+      Math.round(elem.fDeltaPlan * 100)
    ];
 });
 
